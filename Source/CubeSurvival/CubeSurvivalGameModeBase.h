2d999270c0345740ee218f0cdaa4795869fbb5d9
신창훈 <shincchh@gmail.com>
2019-07-05 16:40:33 +0900

    Initial commit

---
+++ Source/CubeSurvival/CubeSurvivalGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubeSurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBESURVIVAL_API ACubeSurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
